639d6a424b4ae340d75ec142a4db424cc00fa620
Luca Alexandre <lucaalexandrepp@gmail.com>
2020-03-12 22:29:43 -0300

    Manutençao

--- LucaAlexandre_RM82617.py
+++ LucaAlexandre_RM82617.py
@@ -1,3 +1,4 @@
+#Lista para percorrer o Alfabeto
 alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 nome = list(input('Digite o nome: '))
 salto = int(input('Valor do salto: '))
